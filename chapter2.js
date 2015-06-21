57ae2dd2589e97ae52a3bf8039cdf42459532911
jheron <jheron@gmail.com>
2015-06-21 13:45:55 -0500

    First commit

---
+++ chapter2.js
@@ -0,0 +1,13 @@
+// chapter2.js
+
+/*
+ comment! But be careful */ // */ is legit in a regular expression
+
+/* This is a different comment
+*/
+
+document.writeln("only floats 1 === 1.0 is " + (1 === 1.0));
+document.writeln("100 == 10e2 " 10e2);
+infinity2 = 1.79769313486231570e+308 + 1.79769313486231570e+308;
+document.writeln("Infinity: " + infinity2 );
+document.writeln("not a number: " + isNaN(NaN) + " " + NaN);
---
+++ program.js
@@ -0,0 +1,10 @@
+var empty_object = {};
+document.writeln("empty_object: " + empty_object + " typeof(): " + typeof(empty_object));
+
+var stooge = {
+	"first-name": "Jerome",
+	"last-name": "Howard"
+};
+
+document.writeln(stooge);
+document.writeln(stooge.first-name);
